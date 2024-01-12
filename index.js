359e0419a484861b54bb260617ff61464ec1a145
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-01-12 14:09:29 +0000

    Use asyn function to deal with the issue of several separate prompts. remove the write file function

--- index.js
+++ index.js
@@ -11,134 +11,87 @@ let test = '';
 let githubUsername = '';
 let emailAddress = '';
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Title of the project? ',
-            name: 'title',
-        }])
-    .then((response) => {
-        console.log(`Project title: ${response.title}`);
-        title = response.title
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+async function prompt() {
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter a description: ',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'Enter your table of contents: ',
-            name: 'tcontents',
-        },
-        {
-            type: 'input',
-            message: 'Enter installation notes: ',
-            name: 'installNotes',
-        },
-        {
-            type: 'input',
-            message: 'Enter usage comments: ',
-            name: 'usage',
-        },
-        {
-            type: 'input',
-            message: 'Enter your license: ',
-            name: 'license',
-            choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License (GPL)', 'BSD 2Clause "Simplified" License', 'BSD 3-Clas "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0', 'Eclipse Public License 2.0']
-        },
-        {
-            type: 'input',
-            message: 'Who are the contributers (blank if only you): ',
-            name: 'contributing',
-        },
-        {
-            type: 'input',
-            message: 'Enter test instructions: ',
-            name: 'test',
-        },
-        {
-            type: 'input',
-            message: 'Enter your questions: ',
-            name: 'questions',
-        },
-        {
-            type: 'input',
-            message: 'Enter your GitHub Username: ',
-            name: 'github',
-        },
-        {
-            type: 'input',
-            message: 'Enter your email address: ',
-            name: 'email',
-        },
+    const response = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Title of the project? ',
+                name: 'title',
+            }])
+        .then((response) => {
+            title = response.title
+            console.log(`Project title: ${title}`);
+        })
 
-    ])
-    .then((response) => {
-        description = response.description;
-        installation = response.installNotes;
-        usage = response.usage;
-        contributing = response.contributing;
-        test = response.test;
-        license = response.license;
-        githubUsername = response.github;
-        emailAddress = response.email;
-    });
+    const response2 = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter a description: ',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'Enter your table of contents: ',
+                name: 'tcontents',
+            },
+            {
+                type: 'input',
+                message: 'Enter installation notes: ',
+                name: 'installNotes',
+            },
+            {
+                type: 'input',
+                message: 'Enter usage comments: ',
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: 'Enter your license: ',
+                name: 'license',
+                choices: ['MIT License', 'Apache License 2.0', 'GNU General Public License (GPL)', 'BSD 2Clause "Simplified" License', 'BSD 3-Clas "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0', 'Eclipse Public License 2.0']
+            },
+            {
+                type: 'input',
+                message: 'Who are the contributers (blank if only you): ',
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: 'Enter test instructions: ',
+                name: 'test',
+            },
+            {
+                type: 'input',
+                message: 'Enter your questions: ',
+                name: 'questions',
+            },
+            {
+                type: 'input',
+                message: 'Enter your GitHub Username: ',
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: 'Enter your email address: ',
+                name: 'email',
+            },
 
-const generateREADME = function () {
-    fs.writeFile('README2.md',
-        `
-# ${title}
+        ])
+        .then((response) => {
+            description = response.description;
+            installation = response.installNotes;
+            usage = response.usage;
+            contributing = response.contributing;
+            test = response.test;
+            license = response.license;
+            githubUsername = response.github;
+            emailAddress = response.email;
+        });
 
-## Description
-
-${description}
-    
-## Table of Contents
-    
-${tcontents}
-
-## Installation
-
-${installNotes}
-
-## Usage
-
-${usage}
-
-## License
-
-${license}
-
-## Contributing
-
-${contributing}
-
-## Tests
-
-${test}
-
-## Questions
-
-${questions}
-GitHub link: http://github.com/${githubUsername}
----
-
-© 2024 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
-    `, (err) => {
-        if (err) {
-            console.log('Error:', err);
-        } else {
-            console.log('File written successfully!');
-        }
-    })
 }
 
-generateREADME()
+prompt()
+
